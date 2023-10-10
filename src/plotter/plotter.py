6cee95154a5d3ab5bdc9669080db96363adc7e9c
@iwliev.roman <iwliev.roman@gmail.com>
2023-10-11 00:33:21 +0300

    added class Plotter settings

--- src/plotter/plotter.py
+++ src/plotter/plotter.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from scipy.interpolate import interp1d
 import numpy as np
+from plotter_settings import PlotterSettings
 
 
 class Plotter:
