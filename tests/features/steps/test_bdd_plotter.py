f70c65dd0521796a851992d2e8344e9038a75d39
@iwliev.roman <iwliev.roman@gmail.com>
2023-10-11 00:20:47 +0300

    added BDD tests for method create_plot from class Plotter

---
+++ tests/features/steps/test_bdd_plotter.py
@@ -0,0 +1,35 @@
+from behave import given, when, then
+
+
+@given('there is a Plotter object')
+def create_currency_plot(context):
+    context.currency_plot = Plotter([1, 2, 3], [4, 5, 6])
+
+
+@when('create_plot method is called')
+def call_create_plot(context):
+    context.plot = context.currency_plot._Plotter__create_plot()
+
+
+@then('graph should be created without errors')
+def check_plot_created(context):
+    assert context.plot is not None, \
+        "Plotter was not created"
+
+
+@then('past values list must be correct')
+def check_x_labels(context):
+    assert context.currency_plot.get_ex_rate_values() == [1, 2, 3], \
+        "Exchange rate values list is not correct"
+
+
+@then('future predictions list must be correct')
+def check_x_labels(context):
+    assert context.currency_plot.get_predicted_ex_rate_values() == [3, 4, 5, 6], \
+        "Predicted exchange rate values list is not correct"
+
+
+@then('figure must contain any graph')
+def check_x_labels(context):
+    assert context.currency_plot.fig.axes is not None, \
+        "Figure contains no graphics"
