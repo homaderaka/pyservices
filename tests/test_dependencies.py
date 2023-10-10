3a62302f7f96ba41328c7fdb7798fc1ec143be82
homaderaka <homaderaka@example.com>
2023-10-10 23:45:45 +0300

    adding tests: dependency check

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -4,7 +4,7 @@ from src.plotter.plotter import Plotter
 
 
 class TestDependency(unittest.TestCase):
-
+    # Тест, чтобы проверить, не ломаются ли зависимости при импорте из src
     def test_plotter(self):
-        p = Plotter([1, 2, 3], [4, 5, 6])
-        assert self.assertIsNotNone(p)
+        p = Plotter([1, 2, 3], [4, 5, 6]).get_plot()
+        self.assertIsNotNone(p)
